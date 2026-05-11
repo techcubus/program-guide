89c61485b5a8f1c9729badfb5b4230d9b919b118
Lily the Elder <137131348+techcubus@users.noreply.github.com>
2026-05-11 02:28:41 -0500

    Add ready method to channel helper

--- js/helpers/channel.js
+++ js/helpers/channel.js
@@ -15,6 +15,10 @@ class Channel {
         console.log('Showing channel');
     }
 
+    ready() {
+        console.log('Readying channel');
+    }
+
     teardown() {
         console.log('Tearing down');
         for (var i in this.timeouts) {
